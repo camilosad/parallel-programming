4757b54da02bc57531408d12f21bcc4d041f63fb
Nichollas Nunes Marvila <nichollasmarvila@gmail.com>
2017-07-20 17:07:29 -0300

    Update T2-Multithreading.rb
    Alterando código para refletir o que é solicitado no enunciado do trabalho.

--- T2-Multithreading.rb
+++ T2-Multithreading.rb
@@ -4,7 +4,7 @@ threads = []
 
 4.times do |i|
    threads[i] = Thread.new {
-      tempo = rand(20)
+      tempo = rand(1..20)
       puts "Eu sou a thread #{i} e vou dormir por #{tempo} segundos!"
       sleep(tempo)
       puts "Eu sou a thread #{i}. Já se passaram #{tempo} segundos e eu serei finalizada!"
